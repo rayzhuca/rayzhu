a015f8a310056c3dbd3af33b5457b2899cd92c64
rayzhuca <raybipse@gmail.com>
2024-11-09 20:44:28 -0500

    Minor CSS fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import miku from "@/public/imgs/miku.jpg";
 
 export default function Page() {
     return (
@@ -10,11 +11,11 @@ export default function Page() {
                         <p className="text-gray-200">junior at uc davis, mathematics and computer science</p>
                     </div>
                     <div className="w-32 h-32 relative mt-12">
-                        <Image src="/imgs/miku.jpg" fill={true} alt="miku" />
+                        <Image src={miku} alt="miku" />
                     </div>
                 </div>
 
-                <div className="w-full h-64"></div>
+                <div className="w-full h-52"></div>
 
                 <div className="flex flex-col">
                     <h2 className="text-lg font-medium">favorite classes</h2>
@@ -25,21 +26,15 @@ export default function Page() {
                     </div>
                 </div>
 
-                <div className="flex flex-col mt-4">
-                    <h2 className="text-lg font-medium">book shelf</h2>
-                    <div className="flex flex-col gap-1 text-gray-200">
-                        <div className="flex justify-between">
-                            <p>designing data intensive applications</p>
-                            <div>27 / 544</div>
-                        </div>
-                        <div className="flex justify-between">
-                            <p>operating systems: three easy pieces</p>
-                            <div>0 / 686</div>
-                        </div>
-                        <div className="flex justify-between">
-                            <p>introduction to algorithms</p>
-                            <div>0 / 1136</div>
-                        </div>
+                <div className="mt-4 mb-8">
+                    <h2 className="text-lg font-medium">Book Shelf</h2>
+                    <div className="grid grid-cols-[2fr,1fr] gap-4 text-gray-200 mt-2">
+                        <p className="w-auto">Designing Data Intensive Applications</p>
+                        <div className="text-right">27 / 544</div>
+                        <p className="w-auto">Operating Systems: Three Easy Pieces</p>
+                        <div className="text-right">0 / 686</div>
+                        <p className="w-auto">Introduction to Algorithms</p>
+                        <div className="text-right">0 / 1136</div>
                     </div>
                 </div>
             </div>
