a2172128398473e16311bfa618982fec7181523b
rayzhuca <raybipse@gmail.com>
2024-11-09 21:30:22 -0500

    Minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -8,18 +8,19 @@ export default function Page() {
                 <div className="flex justify-between">
                     <div className="flex flex-col mt-12">
                         <h1 className="text-2xl font-medium">ray zhu</h1>
-                        <p className="text-gray-200">junior at uc davis, mathematics and computer science</p>
+                        <p className="text-gray-200">junior at uc davis, computer science and mathematics</p>
+                        <p>i want to learn as much as possible so ai wont replace me</p>
                     </div>
                     <div className="w-32 h-32 relative mt-12">
                         <Image src={miku} alt="miku" />
                     </div>
                 </div>
 
-                <div className="w-full h-52"></div>
+                <div className="w-full mt-4 h-48"></div>
 
                 <div className="flex flex-col">
                     <h2 className="text-lg font-medium">favorite classes</h2>
-                    <div className="flex flex-col gap-1 text-gray-200">
+                    <div className="flex flex-col gap-1 text-gray-200 mt-1">
                         <p>ecs 170 -- artifical intelligence</p>
                         <p>mat 135b -- stochastic processes</p>
                         <p>phy 9c -- eletricity and magnetism</p>
@@ -27,13 +28,13 @@ export default function Page() {
                 </div>
 
                 <div className="mt-4 mb-8">
-                    <h2 className="text-lg font-medium">Book Shelf</h2>
-                    <div className="grid grid-cols-[2fr,1fr] gap-4 text-gray-200 mt-2">
-                        <p className="w-auto">Designing Data Intensive Applications</p>
+                    <h2 className="text-lg font-medium">reading progress</h2>
+                    <div className="grid grid-cols-[2fr,1fr] gap-1 text-gray-200 mt-1">
+                        <p className="w-auto">designing data intensive applications</p>
                         <div className="text-right">27 / 544</div>
-                        <p className="w-auto">Operating Systems: Three Easy Pieces</p>
+                        <p className="w-auto">operating systems: three easy pieces</p>
                         <div className="text-right">0 / 686</div>
-                        <p className="w-auto">Introduction to Algorithms</p>
+                        <p className="w-auto">introduction to algorithms</p>
                         <div className="text-right">0 / 1136</div>
                     </div>
                 </div>
