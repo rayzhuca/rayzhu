5996f4b8baa3ff302639218a8e1eff40484c8aa3
rayzhuca <raybipse@gmail.com>
2024-12-21 23:35:25 -0800

    Made note wider in mobile

--- components/note/Note.tsx
+++ components/note/Note.tsx
@@ -7,11 +7,11 @@ interface NoteProps {
 export const Note: React.FC<NoteProps> = ({ title, date, children }) => {
     return (
         <div className="flex flex-col items-center w-full p-8 gap-4">
-            <div className="w-2/3 mt-5">
+            <div className="w-full sm:w-2/3 mt-5">
                 <h1>{title}</h1>
                 <div className="mt-1">{date}</div>
             </div>
-            <main className="w-2/3 flex flex-col gap-2">{children}</main>
+            <main className="w-full sm:w-2/3 flex flex-col gap-2">{children}</main>
         </div>
     );
 };
