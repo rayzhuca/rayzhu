74c5014d859b7c0d2a55f591fdf5f1552cc14b98
rayzhuca <raybipse@gmail.com>
2025-04-26 16:54:51 -0700

    picture gallery

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
 import Image from "next/image";
 import miku from "@/public/imgs/miku.jpg";
+import mazda from "@/public/imgs/mazda.jpg";
+import copyright from "@/public/imgs/copyright.gif";
+import manifold from "@/public/imgs/manifold.gif";
+import bully from "@/public/imgs/bully.png";
+import loringtu from "@/public/imgs/loringtu.jpg";
+import street from "@/public/imgs/street.jpg";
+import mikasa from "@/public/imgs/mikasa.jpg";
+import racing from "@/public/imgs/racing.jpg";
+import somedayfree from "@/public/imgs/somedayfree.jpg";
+import argo from "@/public/imgs/argo.jpg";
+import noise from "@/public/imgs/noise.avif";
 import Link from "next/link";
 
 export default function Page() {
@@ -17,8 +28,6 @@ export default function Page() {
                     </div>
                 </div>
 
-                <div className="w-full mt-4 h-44"></div>
-
                 <div className="flex flex-col">
                     <h2 className="text-lg font-medium">Notes</h2>
                     <div className="flex flex-col gap-1 text-gray-200 mt-1">
@@ -29,14 +38,118 @@ export default function Page() {
                     </div>
                 </div>
 
-                <div className="flex flex-col mt-4">
+                <div className="h-20"></div>
+
+                <div
+                    className="relative border border-gray-100 [&_img]:border [&_img]:border-gray-300"
+                    style={{ width: "100%", height: "300px" }}
+                >
+                    {/* Background image */}
+                    <div className="absolute inset-0" style={{ zIndex: -10 }}>
+                        <Image src={racing} alt="racing background" className="w-full h-full object-cover opacity-40" fill />
+                    </div>
+
+                    {/* Mazda - large image in top left */}
+                    <div className="absolute" style={{ left: 0, top: -10, width: "50%", height: "200px", zIndex: 20 }}>
+                        <Image src={mazda} alt="mazda" className="w-full h-full object-contain" fill />
+                    </div>
+
+                    {/* Argo - top right */}
+                    <div className="absolute" style={{ left: "50%", top: -20, width: "50%", height: "150px", zIndex: 30 }}>
+                        <Image src={argo} alt="argo" className="w-full h-full object-contain" fill />
+                    </div>
+
+                    {/* Manifold - top far right */}
+                    <div
+                        className="absolute"
+                        style={{ left: "80%", top: 30, width: "25%", height: "130px", zIndex: 40, marginLeft: "-24px" }}
+                    >
+                        <Image src={manifold} alt="manifold" className="w-full h-full object-contain" fill />
+                    </div>
+
+                    {/* Bully - middle far right */}
+                    <div
+                        className="absolute"
+                        style={{
+                            left: "65%",
+                            top: "160px",
+                            width: "25%",
+                            height: "200px",
+                            zIndex: 20,
+                            marginLeft: "-40px",
+                            marginTop: "-32px",
+                        }}
+                    >
+                        <Image src={bully} alt="bully" className="w-full h-full object-contain" fill />
+                    </div>
+
+                    {/* Loring Tu - bottom left */}
+                    <div
+                        className="absolute"
+                        style={{ left: -20, top: "220px", width: "25%", height: "160px", zIndex: 30, marginTop: "-32px" }}
+                    >
+                        <Image src={loringtu} alt="loring tu" className="w-full h-full object-contain" fill />
+                    </div>
+
+                    {/* Someday Free - bottom left-center */}
+                    <div
+                        className="absolute"
+                        style={{
+                            left: "35%",
+                            top: "200px",
+                            width: "25%",
+                            height: "160px",
+                            zIndex: 30,
+                            marginLeft: "-24px",
+                            marginTop: "-32px",
+                        }}
+                    >
+                        <Image src={somedayfree} alt="someday free" className="w-full h-full object-contain" fill />
+                    </div>
+
+                    {/* Street - bottom right-center */}
+                    <div
+                        className="absolute"
+                        style={{
+                            left: "35%",
+                            top: "40px",
+                            width: "35%",
+                            height: "300px",
+                            zIndex: 10,
+                            marginLeft: "-40px",
+                            marginTop: "-32px",
+                        }}
+                    >
+                        <Image src={street} alt="street" className="w-full h-full object-contain" fill />
+                    </div>
+
+                    {/* Mikasa - bottom far right */}
+                    <div
+                        className="absolute"
+                        style={{
+                            left: "75%",
+                            top: "200px",
+                            width: "25%",
+                            height: "100px",
+                            zIndex: 40,
+                            marginLeft: "-24px",
+                            marginTop: "16px",
+                        }}
+                    >
+                        <Image src={mikasa} alt="mikasa" className="w-full h-full object-contain" fill />
+                    </div>
+                </div>
+
+                <div className="h-12"></div>
+
+                {/* <div className="flex flex-col mt-4">
                     <h2 className="text-lg font-medium">Favorite classes</h2>
                     <div className="flex flex-col gap-1 text-gray-200 mt-1">
                         <p>ECS 170 -- artifical intelligence</p>
                         <p>MAT 135B -- stochastic processes</p>
                         <p>PHY 9C -- eletricity and magnetism</p>
                     </div>
-                </div>
+                </div> */}
 
                 <div className="mt-4 mb-8">
                     <h2 className="text-lg font-medium">Book Shelf</h2>
---
+++ components/MusicElement.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useRef, useState } from "react";
+import Image from "next/image";
+
+interface MusicElementProp {
+    img_src: string;
+    data_sound: string;
+    className?: string;
+}
+
+const MusicElement: React.FC<MusicElementProp> = () => {
+    return <div></div>;
+};
+
+export default MusicElement;
